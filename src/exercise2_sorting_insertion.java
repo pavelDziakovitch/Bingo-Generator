5eef71e4b1f42f5a0579e0779dd55af170251b7b
Pavel <dziakovitch.pavel@gmail.com>
2021-12-16 19:46:00 +0100

    finished insertion sorting (Stable , n²)

--- src/exercise2_sorting_insertion.java
+++ src/exercise2_sorting_insertion.java
@@ -4,31 +4,40 @@ public class exercise2_sorting_insertion {
 
     public static void main(String[] args) {
 
-        int beeingSorted = 0;
         Random randomGen = new Random();
+        int tempNumber;
+
 
     //Create and Fill Array
-        int[] numbers = new int[100];
+        int[] numbers = new int[10];
         for (int i = 0; i < numbers.length;i++){
             numbers[i] = randomGen.nextInt(10);
         }
     //
+
+    //Output unsorted String
         for(int i = 0; i < numbers.length; i++){
             System.out.print(numbers[i] + "  ");
         }
         System.out.println("");
+    //
 
     //SORT
+
         for(int position = 0; position < numbers.length;position++ ){
-            for(int currentPosition = position; currentPosition >= 1 && numbers[currentPosition] < numbers[currentPosition-1] ;currentPosition--){
-                int temp = numbers[currentPosition-1];
+            for(int currentPosition = position; currentPosition != 0 && numbers[currentPosition] < numbers[currentPosition-1];currentPosition--){
+                tempNumber = numbers[currentPosition-1];
                 numbers[currentPosition-1] = numbers[currentPosition];
-                numbers[currentPosition] = temp;
+                numbers[currentPosition] = tempNumber;
             }
-            
         }
-        for (int number : numbers) {
-            System.out.print(number + "  ");
+    //
+
+    // Output for sorted arrays
+        for(int k = 0; k < numbers.length; k++){
+            System.out.print(numbers[k] + "  ");
         }
+    //
+
     }
 }
